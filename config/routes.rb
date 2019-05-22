fc59b5614e8842eb88c280253819a9cb5df86eb7
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-22 08:42:15 -0600

    Adds about controller and empty index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index', as: 'welcome'
+  get '/about', to: 'about#index', as: 'about'
 
   post '/login', to: 'session#create', as: 'login'
   get '/logout', to: 'session#destroy', as: 'logout'
