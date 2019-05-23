8bb39ac5a158cd202d876bdf2888f053e0ace8a6
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-22 22:54:48 -0600

    Adds update method for teacher with lots of conditionals - need to break down into helper methods

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -8,10 +8,47 @@ class TeachersController < ApplicationController
     elsif params[:update] && params[:update] == "reset"
       @edit_fields = ["New Password", "Confirm New Password"]
     else
-      @edit_fields = ["First Name", "Last Name", "Email"]
+      @edit_fields = ["first_name", "last_name", "email"]
     end
   end
 
+  def update
+    @teacher = Teacher.find(current_user.id)
+    if params[:teacher]["Old Password"]
+      if confirm_params["Old Password"] == @teacher.password && confirm_params["New Password"] == confirm_params["Confirm New Password"]
+        @teacher.update(confirm_params)
+        flash[:success] = "Successfully Reset Password"
+      else
+        flash.now[:error] = "Passwords don't match"
+        render :edit
+      end
+    elsif params[:teacher]["New Password"]
+      if confirm_params["New Password"] == confirm_params["Confirm New Password"]
+        @teacher.update(reset_params)
+        flash[:success] = "Successfully Reset Password"
+      else
+        flash.now[:error] = "Passwords don't match"
+        render :edit
+      end
+    else
+      @teacher.update(general_params)
+      flash[:success] = "Successfully Updated Account Information"
+    end
+    redirect_to account_path
+  end
 
+  private
+
+  def general_params
+    params.require(:teacher).permit(:first_name,:last_name, :email)
+  end
+
+  def reset_params
+    params.require(:teacher).permit("New Password", "Confirm New Password")
+  end
+
+  def confirm_params
+    params.require(:teacher).permit("Old Password","New Password", "Confirm New Password")
+  end
   # probably need various strong params for different update types
 end
