ed1548c8e5d96a549de790a95122fd44c5e5e095
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-23 11:34:06 -0600

    Changes 'token' to 'google_id'

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
-  validates_presence_of :first_name, :last_name, :token, :student_id
+  validates_presence_of :first_name, :last_name, :google_id, :student_id
   validates_uniqueness_of :student_id
 
   has_many :student_courses
