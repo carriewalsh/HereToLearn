0fb4d74617c3c40b56bdeda01e65de9e942bfec2
wipegup <wipegup@gmail.com>
2019-05-22 21:08:39 -0600

    Add google_id to student factory

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     student_id { Faker::Number.number(6) }
+    google_id {Faker::Number.number(10)}
     token { Faker::Crypto.md5 }
   end
 end
