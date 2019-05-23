2ac6933b0d2becdf93a73726d38a4c85b2d29cda
wipegup <wipegup@gmail.com>
2019-05-22 22:17:39 -0600

    Fix mocking of oauth return

--- spec/features/students/log_on_with_google_spec.rb
+++ spec/features/students/log_on_with_google_spec.rb
@@ -4,27 +4,27 @@ describe 'A student with their e-mail in the system' do
 
   it 'can log on with google account via OAuth' do
     user = create(:student)
-
+    # binding.pry
     stub_omniauth(user.google_id, user.first_name, user.last_name)
     visit '/student'
     expect(current_path).to eq(class_code_path)
     expect(page).to have_content("Successfully Logged-In")
 
     # Cleanup
-    OmniAuth.config.mock_auth[:google] = nil
+    OmniAuth.config.mock_auth[:google_oauth2] = nil
   end
 
   it 'cannot log on with information not in student table' do
 
     user = attributes_for(:student)
 
-    stub_omniauth(user.google_id, user.first_name, user.last_name)
+    stub_omniauth(user[:google_id], user[:first_name], user[:last_name])
     visit '/student'
     expect(current_path).to eq(unregistered_path)
     expect(page).to have_content("You are not registered in our system, please contact your teacher")
 
     # Cleanup
-    OmniAuth.config.mock_auth[:google] = nil
+    OmniAuth.config.mock_auth[:google_oauth2] = nil
   end
 
 
@@ -33,14 +33,14 @@ end
 def stub_omniauth(id, first_name, last_name)
   google_id = id
   name = first_name + " " + last_name
-
+  # binding.pry
   OmniAuth.config.test_mode = true
-  OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
     uid: google_id,
     info: {
       name: name,
       email: "example@gmail.com"
-    }
+    }}
   )
 
   Rails.application.env_config['omniauth.auth'] =
