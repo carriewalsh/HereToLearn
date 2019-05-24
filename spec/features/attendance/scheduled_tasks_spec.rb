92b402f7ee61f138d1ed473dae97e98f84b63f1b
wipegup <wipegup@gmail.com>
2019-05-24 09:59:02 -0600

    Skeleton of test for attendance scheduling

---
+++ spec/features/attendance/scheduled_tasks_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'A course is created' do
+  before :each do
+
+  end
+
+  it 'has a 2:00am job scheduled to create attendance_records for all week days'
+
+  it 'has a task scheduled 5 minutes after the course start to mark students absent'
+
+end
