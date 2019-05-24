dd209a45a3202bbd6bef1ddf17b3f1dc6c174676
wipegup <wipegup@gmail.com>
2019-05-23 19:45:50 -0600

    Outline Question Facade

---
+++ app/facades/question_facade.rb
@@ -0,0 +1,17 @@
+class QuestionFacade
+  def initialize(question_list)
+    @question_list = question_list
+  end
+
+  def questions
+
+  end
+
+  private
+
+  def question_data
+  end
+
+  def question_service
+  end
+end
