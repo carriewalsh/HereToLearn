096b72e506d046f178df8a0491acdd783a21c0fb
wipegup <wipegup@gmail.com>
2019-05-23 20:53:23 -0600

    Clean up files

--- app/controllers/student/response_controller.rb
+++ app/controllers/student/response_controller.rb
@@ -1,6 +1,7 @@
 class Student::ResponseController < ApplicationController
   def create
     # Submit to API with post
+    # binding.pry
     flash[:info] = "Thank You!"
     redirect_to student_completed_survey_path
   end
--- spec/features/students/fill_out_survey_spec.rb
+++ spec/features/students/fill_out_survey_spec.rb
@@ -12,6 +12,7 @@ describe 'A student with their e-mail in the system' do
 
     visit '/student'
     fill_in :code, with: @in_class_code.code
+    
     endpoint = '/api/v1/questions?ids=1,2'
     domain = 'http://surveyapp.com'
     body =  File.open('./api_responses/questions.json')
@@ -26,36 +27,13 @@ describe 'A student with their e-mail in the system' do
 
   it 'can submit a survey' do
 
-    # binding.pry
-  # with(
-  #   headers: {
-  #  'Accept'=>'*/*',
-  #  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-  #  'User-Agent'=>'Faraday v0.15.4'
-  #   }).
-  # to_return(status: 200, body: '', headers: {})
-    # stub_request(:get, domain + endpoint).to_return(body: body)
-
-    # save_and_open_page
     choose "1"
     choose "5"
 
     click_on "Submit"
-    # binding.pry
+
     expect(current_path).to eq(student_completed_survey_path)
     expect(page).to have_content("Thank You!")
-    # At this point there is a course_id and student_id in the session
 
-    # Ultimately, I would like to pass the `course_id` to sinatra and recieve the list of Q&As
-    # For the moment, pass a hard-coded list of question IDs
-
-    # From that list of questions, create a Facade to pass to the view
-    # The facade will have a list of questions in `questions`.
-    # Each question has "text", "id" and list of "choices"
-    # If there is only one choice, give a text box
-    # if there are multiple choices, offer all choices As radio buttons (choices have text and id)
-    # If there aren't any choices, give a text box.
-
-    # Give a submit button
   end
 end
