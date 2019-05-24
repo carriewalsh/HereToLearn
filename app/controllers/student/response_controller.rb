e116d4f25bc56406223138e99d8ecd34f852c224
wipegup <wipegup@gmail.com>
2019-05-24 07:30:11 -0600

    Creating new attendance record upon survey submission

--- app/controllers/student/response_controller.rb
+++ app/controllers/student/response_controller.rb
@@ -2,7 +2,11 @@ class Student::ResponseController < ApplicationController
   def create
     # Submit to API with post
     # post_responses
-    # binding.pry
+
+    attendance_record = find_attendance
+    attendance_record.attendance = "present"
+    attendance_record.save
+    
     flash[:info] = "Thank You!"
     redirect_to student_completed_survey_path
   end
@@ -13,13 +17,20 @@ class Student::ResponseController < ApplicationController
 
   private
 
+  def find_attendance
+    now = DateTime.now()
+    time_range = (now-1.hour..now)
+    Attendance.find_by(student_id: session[:student_id],
+                       course_id: session[:course_id],
+                       created_at: time_range)
+  end
   def post_responses
     domain = 'http://surveyapp.com'
     endpoint = '/response'
 
     Faraday.post domain + endpoint,  headers: headers_for_post
   end
-  
+
   def headers_for_post
     {student_id: session[:student_id],
      course_id: session[:course_id],
