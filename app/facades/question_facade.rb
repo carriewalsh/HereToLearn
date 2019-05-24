7d41fb13b1a2d0ff940d967b19805e37dd9b0d32
wipegup <wipegup@gmail.com>
2019-05-23 19:51:03 -0600

    QuestionFacade Built-out

--- app/facades/question_facade.rb
+++ app/facades/question_facade.rb
@@ -4,14 +4,19 @@ class QuestionFacade
   end
 
   def questions
-
+    question_data.map do |answer_list|
+      Question.new(answer_list)
+    end
   end
 
   private
 
   def question_data
+    @_question_data ||= question_service.questions_by_ids(@question_list)
+    @_question_data.group_by{ |q| q[:id]}
   end
 
   def question_service
+    @_question_service ||= QuestionService.new
   end
 end
