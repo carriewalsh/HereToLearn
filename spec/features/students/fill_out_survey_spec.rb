42bcdaeb8953eb4166e6efee05a9797c754917cd
wipegup <wipegup@gmail.com>
2019-05-23 19:36:30 -0600

    Add test for filling out survey

---
+++ spec/features/students/fill_out_survey_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe 'A student with their e-mail in the system' do
+  before :each do
+    @student = create(:student)
+    stub_omniauth(@student.google_id, @student.first_name, @student.last_name)
+
+    @in_class = create(:course)
+    @student.courses << @in_class
+
+    @in_class_code = create(:code, course: @in_class)
+
+    visit '/student'
+    fill_in :code, with: @in_class_code.code
+    click_on 'Start Survey'
+  end
+
+  after :each do
+    OmniAuth.config.mock_auth[:google_oauth2] = nil
+  end
+
+  it 'can submit a survey' do
+    endpoint = '/api/v1/questions?ids=1,2'
+    domain = 'http://surveyApp.com'
+    body =  File.open('./api_responses/questions.json')
+    stub_request(:get, domain + endpoint).to_return(body: body)
+
+
+    choose :yes
+    choose :usual
+
+    click_on "Submit"
+
+    expect(current_path).to eq(student_completed_survey_path)
+    expect(page).to have_content("Thank You!")
+    # At this point there is a course_id and student_id in the session
+
+    # Ultimately, I would like to pass the `course_id` to sinatra and recieve the list of Q&As
+    # For the moment, pass a hard-coded list of question IDs
+
+    # From that list of questions, create a Facade to pass to the view
+    # The facade will have a list of questions in `questions`.
+    # Each question has "text", "id" and list of "choices"
+    # If there is only one choice, give a text box
+    # if there are multiple choices, offer all choices As radio buttons (choices have text and id)
+    # If there aren't any choices, give a text box.
+
+    # Give a submit button
+  end
+end
