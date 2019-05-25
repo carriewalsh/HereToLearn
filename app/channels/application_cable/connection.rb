a3616b8f562695dbfdb47e888eb3f956006fc324
wipegup <wipegup@gmail.com>
2019-05-25 10:40:44 -0600

    Add code outline for action_cable authentication

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,23 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+
+    # include SessionsHelper
+    #
+    # identified_by :admin_user
+    #
+    # def connect
+    #   self.admin_user = find_verified_user
+    # end
+    #
+    # private
+    #
+    #   def find_verified_user
+    #     # binding.pryxt
+    #     if logged_in?
+    #       current_user
+    #     else
+    #       reject_unauthorized_connection
+    #     end
+    #   end
   end
 end
