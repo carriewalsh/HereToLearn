65f55027de4b2eb144ad9ef95f6ee6454c641e50
wipegup <wipegup@gmail.com>
2019-05-25 15:46:48 -0600

    Switch  for  for authentication - help for authentication with cables

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,21 +3,21 @@ module ApplicationCable
 
     # include SessionsHelper
     #
-    # identified_by :admin_user
-    #
-    # def connect
-    #   self.admin_user = find_verified_user
-    # end
-    #
-    # private
-    #
-    #   def find_verified_user
-    #     # binding.pryxt
-    #     if logged_in?
-    #       current_user
-    #     else
-    #       reject_unauthorized_connection
-    #     end
-    #   end
+    identified_by :admin_user
+
+    def connect
+      self.admin_user = find_verified_user
+    end
+
+    private
+
+      def find_verified_user
+        # binding.pryxt
+        if cookies.signed[:user_id]
+          Teacher.find(cookies.signed[:user_id])
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
