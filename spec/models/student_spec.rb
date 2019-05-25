ad82fe022004f83649f029e7de0081a7ba2147c7
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-25 10:18:44 -0600

    Adds todays_attendance spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,21 @@ describe Student, type: :model do
     it { should have_many :student_courses }
     it { should have_many :courses }
   end
+
+  describe "instance methods" do
+    describe ".todays_attendance" do
+      it "shows the value of TODAY's attendance" do
+        today = DateTime.now.beginning_of_day
+        yesterday = today-1
+
+        student = create(:student)
+        course = student.courses.create(name: "PE", period: "1", start_time: "2019-05-21 8:00:00", end_time: "2019-05-21 9:00:00")
+
+        yesterday_att = student.attendances.create(course_id: course.id, created_at: yesterday, attendance: :absent)
+        today_att = student.attendances.create(course_id: course.id, created_at: today, attendance: :present)
+
+        expect(student.todays_attendance).to eq(:present)
+      end
+    end
+  end
 end
