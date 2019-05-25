c40184955b8aa6bbd0fdc97e01e6efeae9896c36
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-25 17:54:06 -0600

    Adds edit and update actions to PasswordResets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,6 +4,25 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-
+    teacher = Teacher.find(current_user.id)
+    if teacher.authenticate(reset_params["Old Password"])
+      if reset_params["New Password"] == reset_params["Confirm New Password"]
+        teacher.update_attribute(:password, reset_params["New Password"])
+        flash[:notice] = "Successfully Reset Password"
+        redirect_to account_path
+      else
+        flash[:error] = "New Passwords Do Not Match"
+        render :edit
+      end
+    else
+      flash[:error] = "Old Password Incorrect"
+      render :edit
+    end
   end
+
+  private
+
+    def reset_params
+      params.require(:teacher).permit("Old Password", "New Password", "Confirm New Password")
+    end
 end
