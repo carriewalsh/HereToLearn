fcd94c12b2e50e62478fe1cf2fe1fb99c72c09d6
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-25 17:40:04 -0600

    Removes validation for password

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,7 @@
 class Teacher < ApplicationRecord
-  validates_presence_of :first_name, :last_name, :email, :password
+  validates_presence_of :first_name, :last_name, :email
   validates_uniqueness_of :email
-  has_secure_password
+  has_secure_password allow_blank: true
 
   has_many :teacher_courses
   has_many :courses, through: :teacher_courses
