8424789385232fb757dea631079dd66bb52d4d2e
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-25 17:40:36 -0600

    Finishes edit information spec and updates tests for capitalized buttons

--- spec/features/teachers/account_spec.rb
+++ spec/features/teachers/account_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 describe "As a logged-in teacher" do
   describe "when I visit my account page" do
     before :each do
-      @teacher = create(:teacher)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@teacher)
-      visit account_path
+      @teacher = create(:teacher, password: "password")
     end
 
     it "shows me my information" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@teacher)
+      visit account_path
       within ".dynamic-container" do
         expect(page).to have_content("First Name: #{@teacher.first_name}")
         expect(page).to have_content("Last Name: #{@teacher.last_name}")
@@ -17,33 +17,38 @@ describe "As a logged-in teacher" do
       end
     end
 
-    xit "allows me to update my non-password information" do
+    it "allows me to update my non-password information" do
       old_name = @teacher.last_name
       old_email = @teacher.email
-
-      click_link "Edit Information"
+      visit welcome_path
+      fill_in 'session[email]', with: @teacher.email
+      fill_in 'session[password]', with: "password"
+      click_on "LOG IN"
+      visit account_path
+      click_link "EDIT INFORMATION"
 
       expect(page).to_not have_content("password")
       fill_in "teacher[last_name]", with: "Nitro"
       fill_in "teacher[email]", with: "wnitro@example.com"
-      click_on "Submit Changes"
+      click_on "SUBMIT CHANGES"
 
+      expect(current_path).to eq(account_path)
       expect(page).to have_content("Successfully Updated Account Information")
-      expect(page).to have_content("#{@teacher.first_name} #{@teacher.last_name}")
+      expect(page).to have_content("#{@teacher.first_name} Nitro")
       expect(page).to_not have_content("#{@teacher.first_name} #{old_name}")
-      expect(page).to have_content("#{@teacher.email}")
+      expect(page).to have_content("wnitro@example.com")
       expect(page).to_not have_content(old_email)
     end
 
     xit "allows me to update my password" do
       old_password = @teacher.password
 
-      click_link "Reset Password"
+      click_link "RESET PASSWORD"
 
       fill_in "teacher[Old Password]", with: old_password
       fill_in "teacher[New Password]", with: "new_password"
       fill_in "teacher[Confirm New Password]", with: "new_password"
-      click_on "Submit Changes"
+      click_on "SUBMIT CHANGES"
 
       expect(page).to have_content("Successfully Reset Password")
 
