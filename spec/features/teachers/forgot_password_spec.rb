95b065bf5b5350a5b034ef99da575bf65d02ba2f
wipegup <wipegup@gmail.com>
2019-05-26 00:24:53 -0600

    Push errors forward in testing mailing rest password instructions

--- spec/features/teachers/forgot_password_spec.rb
+++ spec/features/teachers/forgot_password_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 describe "As a forgetful teacher, who forgot their password" do
   describe "when I reach the welcome page" do
     it "allows me to send an email to reset my password" do
-      @teacher = create(:teacher, reset_token: "asdf", reset_sent_at: DateTime.now)
+      @teacher = create(:teacher) #, reset_token: "asdf", reset_sent_at: DateTime.now)
       visit welcome_path
-      click_link "Forgot password?"
+      click_link "Forgot Password?"
       expect(current_path).to eq(send_reset_link_path)
 
       fill_in 'Email', with: @teacher.email
