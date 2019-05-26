14faca5e1ce4ec26c5280a9ff7da64a01874ffeb
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-25 18:19:02 -0600

    Adds show action and private method course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,20 @@
 class CoursesController < ApplicationController
+  # should probably have a before action here for safety
   def index
     render locals: {
       facade: CoursesFacade.new(current_user)
     }
   end
+
+  def show
+    render locals: {
+      facade: CourseFacade.new(current_user, course)
+    }
+  end
+
+  private
+
+    def course
+      current_user.courses.find(params[:id])
+    end
 end
