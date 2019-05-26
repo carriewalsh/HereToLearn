c4524c279d1d6b1148a165b907804603a5097019
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-25 21:31:26 -0600

    Creates student facade

---
+++ app/facades/students_facade.rb
@@ -0,0 +1,21 @@
+class StudentsFacade
+  attr_reader :id
+  def initialize(id)
+    @id = id
+  end
+
+  def student
+    Student.find(@id)
+  end
+
+  def schedule
+    list = student.courses.map do |course|
+      "Period #{course.period} - #{course.name}"
+    end
+    list
+  end
+
+  def schedule
+    [student.percent_present, student.percent_absent, student.total_absences]
+  end
+end
