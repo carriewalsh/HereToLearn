958ea337bb8230d2e916b9158fe993e9fb40da9d
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-26 15:42:47 -0600

    Adds password_reset method to mailer

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -1,13 +1,8 @@
 class TeacherMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.teacher_mailer.password_reset.subject
-  #
-  def password_reset
-    @greeting = "Hi"
+  def password_reset(teacher)
+    @teacher = teacher
 
-    mail to: "to@example.org"
+    mail to: teacher.email, subject: "Password reset"
   end
 end
