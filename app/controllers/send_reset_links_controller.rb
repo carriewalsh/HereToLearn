de3fffa48b26290098c2ae9ed8607b794727cfa0
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-25 23:06:02 -0600

    Adds form for email reset adds methods to create action, fixes routing error

--- app/controllers/send_reset_links_controller.rb
+++ app/controllers/send_reset_links_controller.rb
@@ -3,6 +3,20 @@ class SendResetLinksController < ApplicationController
 
   end
 
+  def create
+    binding.pry
+    @user = User.find_by(email: reset_params)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:notice] = "Email has been sent with password reset instructions"
+      redirect_to login_path
+    else
+      flash[:notice] = "Email address not found"
+      render :new
+    end
+  end
+
   def edit
 
   end
@@ -10,4 +24,10 @@ class SendResetLinksController < ApplicationController
   def update
 
   end
+
+  private
+
+    def reset_params
+      params.require(:password_reset).permit(:email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   get '/password_reset', to: 'password_resets#edit', as: 'password_reset'
   patch '/password_reset', to: 'password_resets#update'
 
-  get '/send_reset_link/new', to: 'send_reset_links#new', as: 'send_reset_link'
+  get '/send_reset_link', to: 'send_reset_links#new', as: 'send_reset_link'
+  post '/send_reset_link', to: 'send_reset_links#create'
   get '/send_reset_link', to: 'send_reset_links#edit', as: 'edit_send_reset_link'
   patch '/send_reset_link', to: 'send_reset_links#update'
 
