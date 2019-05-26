19e7f1cb0ef55eba6048714d26054db2174281d9
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-25 18:05:09 -0600

    Adds test for courses show page

---
+++ spec/features/teachers/courses/courses_show_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "As a logged-in Teacher" do
+  before :each do
+    @teacher = create(:teacher)
+    @course = create(:course)
+    create_list(:student, 4)
+    @teacher.courses << Course.first
+    Student.first.courses << @course
+    Student.second.courses << @course
+    Student.third.courses << @course
+    Student.fourth.courses << @course
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@teacher)
+    visit course_path(Course.first.id)
+  end
+
+  describe "When I visit a courses's show page" do
+    it "shows me the class roster with today's attendance information" do
+      expect(page).to have_content(@course.name)
+      expect(page).to have_css('.student-card', count: 4)
+      expect(page).to have_content("#{student.first.first_name} #{student.first.last_name}")
+    end
+
+    it "has a field and button that allows me to group randomly" do
+
+    end
+  end
+end
