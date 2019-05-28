00a96c4d57b0b330dfe9503fc5454a825739c20d
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-28 10:24:57 -0600

    Adds deactivate action to strategies controller

--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -10,4 +10,11 @@ class StrategiesController < ApplicationController
   def update
 
   end
+
+  def deactivate
+    # binding.pry
+    strategy = Strategy.find(params[:id])
+    strategy.update(active: false)
+    redirect_to student_path(strategy.student_id, course_id: params[:course_id])
+  end
 end
