5d4a07ed53550737c602134bc4952df3bdfe5c8e
JennicaStiehl <jennica.stiehl@gmail.com>
2019-05-29 12:29:52 -0600

    removes pry. tests not passing because seed data across apps are not yet alligned.

--- app/facades/student_facade.rb
+++ app/facades/student_facade.rb
@@ -32,11 +32,9 @@ class StudentFacade
   end
 
   def prediction_results
-    binding.pry
     conn = Faraday.new("http://lit-fortress-28598.herokuapp.com/machinelearning/results/?student_id=#{student.student_id}")
     results = conn.get
     data = JSON.parse(results.body, symbolize_names: true)
-    # {:score=>64.1, :meals_missed=>1.0, :sleep_quality=>{:none=>0.0, :less_than=>1.0, :usual=>0.0, :more_than=>0.0, :way_more=>0.0}}
     Prediction.new(data)
   end
 end
