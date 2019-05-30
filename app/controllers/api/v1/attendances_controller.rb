ca4f7b91f3f7fcce3dbdcfd541ff48912ed49277
wipegup <wipegup@gmail.com>
2019-05-30 15:37:23 -0600

    js update attendance functioning

--- app/controllers/api/v1/attendances_controller.rb
+++ app/controllers/api/v1/attendances_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::AttendancesController < ApplicationController
   skip_before_action :verify_authenticity_token
   def update
-    attendance = Attendance.find_by(course_id: params[:course_id], student_id: params[:student_id], created_at: [DateTime.now.midnight, DateTime.now])
+    # binding.pry
+    attendance = Attendance.find_by(course_id: params[:course_id], student_id: params[:student_id], created_at: (Time.current - 24.hours..Time.current))
+    # binding.pry
     attendance.update_attribute(:attendance, params[:attendance])
   end
 end
