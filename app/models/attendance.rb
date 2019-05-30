a58e6e356bd000c4b2d9edd7e753d301728bdfc9
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-29 19:06:55 -0600

    Adds attendance percent this date method and spec for Attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,4 +3,11 @@ class Attendance < ApplicationRecord
   belongs_to :course
 
   enum attendance: [:absent, :present, :tardy, :present_no_response, :absent_with_response]
+
+  def attendance_percent_this_date
+    days = Attendance.where("created_at < ?", self.created_at + 1.day)
+    total = days.count
+    present = days.where(attendance: ["present", "present_no_response"]).count
+    (100.0 * present / total).round(1)
+  end
 end
--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Attendance, type: :model do
     it { should belong_to :student }
     it { should belong_to :course }
   end
+
+  describe "instance methods" do
+    describe "attendance_percent_this_date" do
+      it "returns the total percent attendance on a given date" do
+
+        today = DateTime.now
+        student = create(:student)
+        course = create(:course)
+        att1 = student.attendances.create(attendance: "present",course_id: course.id, created_at: today - 5.days)
+        att2 = student.attendances.create(attendance: "present",course_id: course.id, created_at: today - 4.days)
+        att3 = student.attendances.create(attendance: "present",course_id: course.id, created_at: today - 3.days)
+        att4 = student.attendances.create(attendance: "absent",course_id: course.id, created_at: today - 2.days)
+        att5 = student.attendances.create(attendance: "absent",course_id: course.id, created_at: today - 1.day)
+        att6 = student.attendances.create(attendance: "absent",course_id: course.id, created_at: today)
+
+        expect(att2.attendance_percent_this_date).to eq(100.0)
+        expect(att4.attendance_percent_this_date).to eq(75.0)
+        expect(att6.attendance_percent_this_date).to eq(50.0)
+      end
+    end
+  end
 end
