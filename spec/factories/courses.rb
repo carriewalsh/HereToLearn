7c95b4dc2f808fac0f9a18c75747720d01018cf3
wipegup <wipegup@gmail.com>
2019-05-30 09:38:05 -0600

    Change factories to reflect format changes

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::IndustrySegments.sector }
     period { Faker::Number.number(1) }
     start_time { (7+rand(7))*100 }
-    end_time { start_time + 50 }
+    end_time { (Time.parse(start_time) + 50.minutes).strftime("%H:%M") }
   end
 end
