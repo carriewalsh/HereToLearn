0f27891dccf9b6a7a6b09988d31fbc17d5ec7e05
wipegup <wipegup@gmail.com>
2019-05-30 09:43:30 -0600

    Fix courses factory

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :course do
     name { Faker::IndustrySegments.sector }
     period { Faker::Number.number(1) }
-    start_time { (7+rand(7))*100 }
+    start_time { (7+rand(7)).to_s + ":00"}
     end_time { (Time.parse(start_time) + 50.minutes).strftime("%H:%M") }
   end
 end
