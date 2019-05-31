f8bfd83314dc3454d8e693bd45ae536cfdaebed5
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-31 07:57:56 -0600

    Fixes find/click section to get correct path

--- spec/features/teachers/statistics/statistics_index_spec.rb
+++ spec/features/teachers/statistics/statistics_index_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "statistics index page" do
   describe "as a logged-in Teacher" do
     before :each do
-      @teacher = create(:teacher)
+      @teacher = create(:teacher, role: 2)
       @course = create(:course)
       create_list(:student, 4)
       @teacher.courses << Course.first
@@ -11,16 +11,20 @@ describe "statistics index page" do
       Student.second.courses << @course
       Student.third.courses << @course
       Student.fourth.courses << @course
+      att1 = Student.first.attendances.create(attendance: "absent",course_id: @course.id, created_at: DateTime.now.midnight)
+      att2 = Student.second.attendances.create(attendance: "present",course_id: @course.id, created_at: DateTime.now.midnight)
+      att3 = Student.third.attendances.create(attendance: "absent",course_id: @course.id, created_at: DateTime.now.midnight)
+      att4 = Student.fourth.attendances.create(attendance: "present",course_id: @course.id, created_at: DateTime.now.midnight)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@teacher)
       visit course_path(Course.first.id)
     end
 
     it "shows bar charts about each individual student" do
-      click_on 'Statistics'
+      find(".navbar", text: 'Statistics').click
+      click_on @course.name
 
       expect(current_path).to eq(statistics_path)
-      save_and_open_page
       expect(page).to have_content('Statistics')
     end
   end
