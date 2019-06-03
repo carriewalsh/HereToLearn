c614d7c53632364c3bc6a7d2cf780e5f2b736679
wipegup <wipegup@gmail.com>
2019-06-02 19:23:52 -0600

    Add conditional if unknown student being added

--- app/assets/javascripts/channels/attendance.js
+++ app/assets/javascripts/channels/attendance.js
@@ -3,8 +3,13 @@
     connected: function() {},
     disconnected: function() {},
     received: function(data) {
+      // debugger;
       // alert("Student with ID" + data.student_id + "Marked"+ data.attendance);
+      if (data.student_id){
       showUpdatedAttendance(data.student_id, data.course_id, data.attendance)
+    }else{
+      addAttendance(data.student_id, data.student_name, data.course_id, data.attendance)
+    }
   }
   });
 
